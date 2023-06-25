f61ac7a1b9e0d170e22dcb85e7d18ba7ef0a7aef
Ruben Calderon <Rcaldeon0907@gmail.com>
2023-06-25 00:11:01 -0700

    .

--- src/index.js
+++ src/index.js
@@ -4,11 +4,40 @@ import "./style.css";
 const ToggleMore = (() => {
   let more = document.querySelector(".more");
   more.addEventListener("click", () => {
-    let extraNav = document.querySelector(".show-more");
-    if (window.getComputedStyle(extraNav, "hidden")) {
-      extraNav.style.visibility = "visible";
-    } else {
-      extraNav.style.visibility = "hidden";
-    }
+    toggleDropDown();
+    toggleArrow();
   });
 })();
+
+const imageFlow = (() => {
+  let image = document.querySelector(".imageSlideContainer");
+  changeImg();
+})();
+
+function changeImg() {
+  let image = document.querySelector(".imageSlideContainer");
+  let image1 = "../src/images/castle.png";
+  let image2 = "../src/images/pyramid.png";
+  let image3 = "../src/images/stonehenge.png";
+  let imageArray = [image1, image2, image3];
+  let slideCounter = 0;
+  console.log(image);
+}
+
+const imageSlider = (() => {
+  let imageSlide = document.querySelector(".imageSlideContainer");
+})();
+
+function toggleDropDown() {
+  let extraNav = document.querySelector(".show-more");
+  extraNav.classList.toggle("visibility");
+}
+
+function toggleArrow() {
+  let arrow = document.querySelector(".arrow");
+  if (arrow.getAttribute("src") === "../src/images/menu-down.png") {
+    arrow.src = "../src/images/menu-up.png";
+  } else {
+    arrow.src = "../src/images/menu-down.png";
+  }
+}
